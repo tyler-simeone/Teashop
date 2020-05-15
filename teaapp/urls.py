e9d5faeb82d7a6467cf9e2de73d622aa66f56d8b
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-15 11:40:37 -0500

    added button in tea list template to add a new tea, uncommented the tea_form view in urls.py, and fixed a typo in tea form template

--- teaapp/urls.py
+++ teaapp/urls.py
@@ -6,5 +6,5 @@ app_name = "teaapp"
 
 urlpatterns = [    
     path('', tea_list, name="tea_list"),
-    # path('/form', tea_form, name="tea_form"),
+    path('/form', tea_form, name="tea_form"),
 ]
