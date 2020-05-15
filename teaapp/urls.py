6b97c1fd75087ce5778e5749773100ae2877be43
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-15 12:02:03 -0500

    added url pattern for tea_details and added links to each tea in the list

--- teaapp/urls.py
+++ teaapp/urls.py
@@ -7,4 +7,5 @@ app_name = "teaapp"
 urlpatterns = [    
     path('', tea_list, name="tea_list"),
     path('/form', tea_form, name="tea_form"),
+    path('/details', tea_details, name="tea_details"),
 ]
