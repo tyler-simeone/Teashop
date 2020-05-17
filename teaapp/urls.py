8a4654f3fe5b128a5873393dc0db81f7e45008cd
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-17 11:54:17 -0500

    added edit view to package. created edit view in form.py. added url pattern for edit view. updated tea form.html to handle an edit submit

--- teaapp/urls.py
+++ teaapp/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-# from teaapp import views
 from .views import *
 
 app_name = "teaapp"
@@ -8,4 +7,5 @@ urlpatterns = [
     path('', tea_list, name="tea_list"),
     path('/form/', tea_form, name="tea_form"),
     path('/<int:tea_id>/', tea_details, name="tea_details"),
+    path('/<int:tea_id>/form/', tea_edit_form, name="tea_edit_form"),
 ]
--- teaapp/views/tea/form.py
+++ teaapp/views/tea/form.py
@@ -1,10 +1,22 @@
 import sqlite3
 from django.shortcuts import render
 from ..connection import Connection
+from .details import get_tea
 
 def tea_form(request):
     if request.method == "GET":
         template = 'tea/form.html'
         context = {}
+
+        return render(request, template, context)
+
+def tea_edit_form(request, tea_id):
+    if request.method == 'GET':
+        tea = get_tea(tea_id)
+
+        template = 'tea/form.html'
+        context = {
+            'tea': tea
+        }
 
         return render(request, template, context)
